8af64705178b5a209646c7c91076ef3463ab35af
Rolando Santamaria Maso <kyberneees@gmail.com>
2017-04-04 15:36:58 +0200

    renaming basic-http.js to koa.js

---
+++ demos/koa.js
@@ -0,0 +1,33 @@
+const HydraServiceFactory = require('./../index').HydraServiceFactory;
+const factory = new HydraServiceFactory({
+    hydra: {
+        'serviceName': 'koa-service-test',
+        'serviceDescription': 'Basic koa service on top of Hydra',
+        'serviceIP': '127.0.0.1',
+        'servicePort': 3000,
+        'serviceType': 'koa',
+        'serviceVersion': '1.0.0',
+        'redis': {
+            'url': '127.0.0.1',
+            'port': 6379,
+            'db': 15
+        }
+    }
+});
+
+factory.on('hydra:registered', async() => {
+    let service = await factory.getService({
+        bootstrap: async(service, factory) => {
+            let router = require('koa-router')();
+            router.get('/v1/welcome', async(ctx) => {
+                ctx.body = 'Hello World!';
+            });
+
+            service.use(router.routes());
+        }
+    });
+
+    console.log(`Service available through hydra as '${factory.config.hydra.serviceName}'`);
+});
+
+factory.init();
