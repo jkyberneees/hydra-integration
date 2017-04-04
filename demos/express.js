356fba9c086b20a6ffc0e1c508392dd96d898a56
Rolando Santamaria Maso <kyberneees@gmail.com>
2017-04-04 12:36:36 +0200

    adding express and hapi examples

---
+++ demos/express.js
@@ -0,0 +1,32 @@
+const HydraServiceFactory = require('./../index').HydraServiceFactory;
+const factory = new HydraServiceFactory({
+    hydra: {
+        'serviceName': 'express-service-test',
+        'serviceDescription': 'Basic express service on top of Hydra',
+        'serviceIP': '127.0.0.1',
+        'servicePort': 3000,
+        'serviceType': 'express',
+        'serviceVersion': '1.0.0',
+        'redis': {
+            'url': '127.0.0.1',
+            'port': 6379,
+            'db': 15
+        }
+    }
+});
+
+factory.on('hydra:registered', async() => {
+    let service = await factory.getService({
+        bootstrap: async(service, factory) => {
+            let router = require('express').Router();
+            router.get('/welcome', (req, res) => res.send('Hello World!'));
+
+            service.use('/v1', router);
+        }
+    });
+
+    require('express-route-log')(service);
+    console.log('Express service running at: http://localhost:' + factory.config.hydra.servicePort);
+});
+
+factory.init();
---
+++ demos/hapi.js
@@ -0,0 +1,37 @@
+const HydraServiceFactory = require('./../index').HydraServiceFactory;
+const factory = new HydraServiceFactory({
+    hydra: {
+        'serviceName': 'hapi-service-test',
+        'serviceDescription': 'Basic express service on top of Hydra',
+        'serviceIP': '127.0.0.1',
+        'servicePort': 3000,
+        'serviceType': 'hapi',
+        'serviceVersion': '1.0.0',
+        'redis': {
+            'url': '127.0.0.1',
+            'port': 6379,
+            'db': 15
+        }
+    }
+});
+
+factory.on('hydra:registered', async() => {
+    let service = await factory.getService({
+        bootstrap: async(service, factory) => {
+            service.route({
+                method: 'GET',
+                path: '/v1/welcome',
+                handler: (request, reply) => {
+                    reply('Hello World!');
+                }
+            });
+
+            return new Promise((resolve, reject) => service.register({
+                register: require('blipp'),
+                options: {}
+            }, (err) => (err) ? reject(err) : resolve()));
+        }
+    });
+});
+
+factory.init();
