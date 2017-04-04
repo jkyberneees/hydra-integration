28bbfd5f7f07b0839ba61a05d73383061d4682b6
Rolando Santamaria Maso <kyberneees@gmail.com>
2017-04-04 12:37:15 +0200

    catching possible exceptions during webserver creating

--- index.js
+++ index.js
@@ -13,57 +13,65 @@ function getEventName(postfix) {
 let serviceStrategies = {
     express: (factory, config) => {
         return new Promise(async(resolve, reject) => {
-            let service = require('express')();
-            service.get('/_health', (req, res) => res.sendStatus(HTTP_OK));
+            try {
+                let service = require('express')();
+                service.get('/_health', (req, res) => res.sendStatus(HTTP_OK));
 
-            if (config.bootstrap) {
-                await config.bootstrap(service, factory);
+                if (config.bootstrap) {
+                    await config.bootstrap(service, factory);
+                }
+
+                // registering hydra routes
+                let routes = service._router.stack.filter(stack => stack.route).map(r => r.route);
+                await hydra.registerRoutes(routes.reduce((arr, route) => {
+                    Object.keys(route.methods).forEach(method => arr.push(`[${method.toUpperCase()}]${route.path}`));
+                    return arr;
+                }, []));
+
+                // starting express server
+                let server = service.listen(config.hydra.servicePort, config.hydra.serviceIP, (err) => err ? reject(err) : resolve(service));
+
+                // registering server.close callback 
+                factory.on(getEventName('beforeShutdown'), () => server.close());
+            } catch (err) {
+                reject(err);
             }
-
-            // registering hydra routes
-            let routes = service._router.stack.filter(stack => stack.route).map(r => r.route);
-            await hydra.registerRoutes(routes.reduce((arr, route) => {
-                Object.keys(route.methods).forEach(method => arr.push(`[${method.toUpperCase()}]${route.path}`));
-                return arr;
-            }, []));
-
-            // starting express server
-            let server = service.listen(config.hydra.servicePort, config.hydra.serviceIP, (err) => err ? reject(err) : resolve(service));
-
-            // registering server.close callback 
-            factory.on(getEventName('beforeShutdown'), () => server.close());
         });
     },
     hapi: (factory, config) => {
         return new Promise(async(resolve, reject) => {
-            const Hapi = require('hapi');
-            const service = new Hapi.Server(config.hapi || {});
-            service.connection({
-                port: config.hydra.servicePort,
-                host: config.hydra.serviceIP
-            });
+            try {
+                const Hapi = require('hapi');
+                const service = new Hapi.Server(config.hapi || {});
+                service.connection({
+                    port: config.hydra.servicePort,
+                    host: config.hydra.serviceIP
+                });
 
-            service.route({
-                method: 'GET',
-                path: '/_health',
-                handler: (request, reply) => reply()
-            });
+                service.route({
+                    method: 'GET',
+                    path: '/_health',
+                    handler: (request, reply) => reply()
+                });
 
-            if (config.bootstrap) {
-                await config.bootstrap(service, factory);
+                if (config.bootstrap) {
+                    await config.bootstrap(service, factory);
+                }
+
+                // registering hydra routes
+                await hydra.registerRoutes(service.table()[0].table.reduce((arr, route) => {
+                    arr.push(`[${route.method.toUpperCase()}]${route.path}`);
+                    return arr;
+                }, []));
+
+                // starting express server
+                service.start(err => err ? reject(err) : resolve(service));
+
+                // registering server.close callback 
+                factory.on(getEventName('beforeShutdown'), () => service.stop());
+            } catch (err) {
+                reject(err);
             }
-
-            // registering hydra routes
-            await hydra.registerRoutes(service.table()[0].table.reduce((arr, route) => {
-                arr.push(`[${route.method.toUpperCase()}]${route.path}`);
-                return arr;
-            }, []));
-
-            // starting express server
-            service.start(err => err ? reject(err) : resolve(service));
-
-            // registering server.close callback 
-            factory.on(getEventName('beforeShutdown'), () => service.stop());
         });
     }
 };
