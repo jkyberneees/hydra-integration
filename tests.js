2e34dc6c4f05e8630ed65b2395f5ad97ca4bcd56
Rolando Santamaria Maso <kyberneees@gmail.com>
2017-04-04 11:53:22 +0200

    adding test cases for hapi strategy

--- tests.js
+++ tests.js
@@ -48,4 +48,53 @@ describe('Hydra Service Factory', () => {
         // finally shutdown all including express server
         return factory.shutdown();
     });
+
+    it('Building hydra service + hapi', async() => {
+        const factory = new HydraServiceFactory({
+            hydra: {
+                'serviceName': 'hapi-service-test',
+                'serviceDescription': 'Basic hapi service on top of Hydra',
+                'serviceIP': '127.0.0.1',
+                'servicePort': 3000,
+                'serviceType': 'hapi',
+                'serviceVersion': '1.0.0',
+                'redis': {
+                    'url': '127.0.0.1',
+                    'port': 6379,
+                    'db': 15
+                }
+            }
+        });
+
+        // initialize the factory
+        let info = await factory.init();
+        // build express service
+        let service = await factory.getService({
+            bootstrap: async(service, factory) => {
+                service.route({
+                    method: 'GET',
+                    path: '/v1/welcome',
+                    handler: (request, reply) => {
+                        reply('Hello World!');
+                    }
+                });
+            }
+        });
+
+        // test express service API directly
+        await request(service.listener).get('/_health').expect(200);
+        await request(service.listener).get('/v1/welcome').then(response => expect(response.text).to.equal('Hello World!'));
+
+        // test express service API directly through hydra
+        let hydra = factory.getHydra();
+        let message = hydra.createUMFMessage({
+            to: 'hapi-service-test:[GET]/v1/welcome',
+            from: 'website:backend',
+            body: {}
+        });
+        await hydra.makeAPIRequest(message).then(response => expect(response.body).to.equal('Hello World!'));
+
+        // finally shutdown all including express server
+        return factory.shutdown();
+    });
 });
