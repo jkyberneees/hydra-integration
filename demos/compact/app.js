86498502a0834593c03c94ce2e0863ba38249eff
Rolando Santamaria Maso <kyberneees@gmail.com>
2017-04-06 12:58:23 +0200

    adding minimalistic demo using koa

---
+++ demos/compact/app.js
@@ -0,0 +1,13 @@
+const HydraServiceFactory = require('./../../index').HydraServiceFactory;
+const router = require('koa-router')();
+const config = require('./config');
+
+new HydraServiceFactory(config)
+    .init()
+    .then(factory => factory.getService(service => {
+        router.get('/v1/welcome', async(ctx) => {
+            ctx.body = 'Hello World!';
+        });
+
+        service.use(router.routes());
+    }));
